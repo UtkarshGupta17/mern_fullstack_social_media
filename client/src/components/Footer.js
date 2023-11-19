6e9f62f986b2f002c2695a85fd3f1536b2848337
VarunShukla07 <varun.shuklaoffical@gmail.com>
2023-11-19 21:51:19 +0530

    Footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,17 @@
+import { Card, Grid, Link, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <Box pb={3}>
+      <Card>
+        <Typography variant="subtitle1">
+          DevMingle is fun with friends so SHARE IT NOW! ⭐
+        </Typography>
+      </Card>
+    </Box>
+  );
+};
+
+export default Footer;
